a9671237561a914c8c50e1121fc5c1f6ed914daa
rustyBilges <tophermcw@gmail.com>
2020-03-23 17:06:48 +0000

    Basic functionality working offline, including static matplotlib plot.

--- rankapp/patient_db.py
+++ rankapp/patient_db.py
@@ -13,17 +13,18 @@ class DummyPatientData(implements(I_PatientData)):
     def __init__(self):
 
         self.df = pd.DataFrame()
-        self.df['Name'] = ['Paul Stephenson', 'Princess Campbell', 'Guy Bailey', 
-		      'Roy Hackett', 'Carmen Beckford', 'Prince Brown', 
-		      'Owen Henry', 'Pero Jones', 'James Peters', 'Alfred Fagon']
-        self.df['Bed'] = [1,2,3,5,7,8,11,12,14,15]
-        self.df['T_number'] = ['T38746', 'T18346', 'T32985', 'T23190', 'T19583',
-                'T49568', 'T30297', 'T43078', 'T89765', 'T34287']
-        self.df['SOFA'] = ['61', '52', '81', '77', '65', '82', '80', '59', '38', '76']
-        self.df['Admission'] = ['2019/01/25', '2019/03/01', '2019/02/18', '2019/02/22', 
-                                '2019/02/15', '2019/02/24', '2019/03/02', '2019/02/21', '2019/02/28', '2019/02/29' ]
-
-
+        self.df['Bed'] = ['GICU 01','GICU 02','CICU 01','CICU 05','CICU 07','GICU 09','GICU 11','CICU 12','GICU 14','CICU 15']
+        self.df['SOFA'] = [16, 5, 8, 17, 6, 18, 20, 5, 8, 7]
+        self.df['VT_kg'] = ['6.0', '7.1', '8.0', '5.5', '6.2', '10.0', '11.0', '5.8', '7.2', '6.0']
+        self.df['FiO2'] = ['0.5', '0.6', '0.4', '0.8', '0.7', '0.6', '0.7', '0.5', '0.8', '0.6' ]
+        self.df['PEEP'] = ['5', '6', '10', '8', '7', '6', '7', '5', '8', '6' ]
+        self.df['pH'] = ['0.5', '0.6', '0.4', '0.8', '0.7', '0.6', '0.7', '0.5', '0.8', '0.6' ]
+        self.df['BE'] = ['0.5', '0.6', '0.4', '0.8', '0.7', '0.6', '0.7', '0.5', '0.8', '0.6' ]
+        self.df['FB'] = ['500 (+1500)', '60 (+2000)', '400 (+500)', '500 (+1000)', '-70 (-1500)', '650 (+1000)', '-70 (-540)', '50 (+2000)', '-80 (+120)', '640 (+2300)' ]
+        self.df['Pos'] = ['prone', 'supine', 'supine', 'prone', 'supine', 'prone', 'supine', 'supine', 'prone', 'supine']
+        self.df['Norad'] = ['0', '0', '0.2', '0.3', '0', '0.1', '0.3', '0.1', '0', '0.4']
+        self.df['COVID19'] = ['sent', 'pending', 'negative', 'positive', 'pending', 'sent', 'positive', 'positive', 'negative', 'pending']
+        
     def returnPatientDf(self):
         return self.df
 
--- rankapp/tables.py
+++ rankapp/tables.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
+import io
+import os
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, current_app
@@ -17,67 +24,90 @@ bp = Blueprint('tables', __name__)
 ##   rather than decorators as currently used here.
 df = pd.DataFrame()
 nrfd = dict()
+patient = None
 
 
 @bp.route('/')
 @login_required
 def index():
-    global df
+    global df, nrfd
     if current_app.config['PATIENTDATA']=='dummy':
         patientData = DummyPatientData()
     elif current_app.config['PATIENTDATA']=='icca':
         patientData = IccaPatientData()
 
     df = patientData.returnPatientDf()
-    nrfd = {name:False for name in df['Name']}
+    nrfd = {bed:False for bed in df['Bed']}
     table_d = json.loads(df.to_json(orient='index'))
     columns = df.columns
                 
     return render_template('tables/table1.html', columns=columns, 
                             table_data=table_d)
 
-@bp.route('/table2')
+@bp.route('/patient_view')
 @login_required
-def table2():
-    global df
-    drop_rows = [i for i,name in enumerate(df['Name']) if nrfd[name]] 
-    df_selected = df.drop(drop_rows, axis=0)
-    df_selected['Rank'] = [i for i in range(len(df_selected))]
-    df_selected = df_selected[['Rank', 'Name', 'Bed', 'T_number', 'Age', 'Admission']]
-    table_d = json.loads(df_selected.to_json(orient='index'))
-    columns = df_selected.columns
-            
-    return render_template('tables/table2.html', columns=columns, 
-                            table_data=table_d)
+def patient_view():
+    global df, patient
+    #df_selected = df
+    img = io.BytesIO()
+    #fig = Figure()
+    
+    plt.plot([1,2,3,4,5,6,7,8,9,10,11,12], [24,22,22,23,19,20,17,15,17,13,10,10])
+    plt.title("SOFA score during ICU admission")
+    plt.xlabel("day of admission")
+    plt.ylabel("SOFA score")
+    plt.ylim([0,24])#
+    plt.xlim([1,12])
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+
+    #plot_url = base64.b64encode(img.getvalue())
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    #pngImage = io.BytesIO()
+    #plt.savefig(bytes_image, format='png')
+    #bytes_image.seek(0)
+    #FigureCanvas(fig).print_png(pngImage)
+    
+    # Encode PNG image to base64 string
+    #pngImageB64String = "data:image/png;base64,"
+    #pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+    
+
+    return render_template('tables/patient_view.html', image='static/images/new_plot.png', patient=patient, plot_url=plot_url)
+    
 
 @bp.route('/logout_msg')
 def finish():
     logout()
     return render_template('tables/logout_msg.html')
 
-@bp.route('/submit_table1', methods=('GET', 'POST'))
+@bp.route('/back_to_dashboard', methods=('GET', 'POST'))
+def back_to_dashboard():
+    return redirect(url_for('tables.index'))
+
+@bp.route('/ward_view', methods=('GET', 'POST'))
+def ward_view():
+    return render_template('tables/ward_view.html')
+
+@bp.route('/select_patient', methods=('GET', 'POST'))
 @login_required
-def submit_table1():
+def select_patient():
     if request.method == 'POST':
-        global df, nrfd
-        for name in df['Name']:    
-            selected = request.form[name]
-            if selected=="selected":
-                nrfd[name] = True
-            elif selected=="unselected":
-                nrfd[name] = False
-       
-        print(nrfd)
-    return redirect(url_for('tables.table2'))
+        global df, patient
+        #patient = request.form.get('selectedPatient')
+        patient = request.form['selectedPatient']
+        print(patient)
+    return redirect(url_for('tables.patient_view'))
 
 @bp.route('/submit_table2', methods=('GET', 'POST'))
 @login_required
 def submit_table2():
     if request.method == 'POST':
         global df, nrfd
-        for name in df['Name']:    
-            if not nrfd[name]:
-                rank = request.form[name]
-                print("%s : %s" %(name,rank))
+        for bed in df['Bed']:    
+            if not nrfd[bed]:
+                rank = request.form[bed]
+                print("%s : %s" %(bed,rank))
         
     return redirect(url_for('tables.finish'))
