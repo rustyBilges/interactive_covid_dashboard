66bc77b6ad9bc7067d5ff239b9814f303ccd002f
McWilliams <Christopher.McWilliams@uhbristol.nhs.uk>
2020-04-12 16:30:49 +0100

    Functional json compiling patient data with schema and previous values. Need to refactor, and styling and increase number of variables in the excel files. Also missing functions to produce derived variables.

--- iicudash/dashboard_data_icca_to_json.py
+++ iicudash/dashboard_data_icca_to_json.py
@@ -1,23 +1,35 @@
 from helper import icca_query
 import pandas as pd
+import json
 
 if __name__=='__main__':
-#    
-#    df_a = pd.read_excel('COVID_interventions_attributes.xlsx', sheet_name='PtAssessment')
-#    #print(df)
-#    
-#    sql = """SELECT P.encounterId as encounterId, P.chartTime, DI.longLabel, DA.shortLabel, P.valueNumber, P.valueString, DI.interventionId as interventionId, DA.attributeId as attributeId FROM PtAssessment P
-#    INNER JOIN D_Intervention DI
-#    ON P.interventionId=DI.interventionId 
-#    INNER JOIN D_Attribute DA
-#    ON P.attributeId=DA.attributeId
-#    WHERE P.encounterId in (SELECT encounterId from PtBedStay where outTime is null and clinicalUnitId in (5,8)) AND DI.interventionId in 
-#    """ + str(tuple(df_a.interventionId)) + " AND DA.attributeId in " + str(tuple(df_a.attributeId))
-#    
-#    ptassessment_results = icca_query(sql)
+#   
+    #with open('patient_data_schema.json', 'r') as read_file:
+    with open('patient_data.json', 'r') as read_file:
+        schema = json.load(read_file) 
     
-    #if ptassessment_results is not None:
-    #    print(ptassessment_results)
+    # This is to store the data that will be saved in the new json file for display on the summary table view of the dashboard
+    data_dict = {'parameters': [], 'values': []}
+
+    # Loop through and get the interventions and attributes that will be selected from each database table, then query the table:
+    #   Note: when this script is scheduled a timeout setting will be added. If the script times out the old json data will be used.
+    database_tables = ['PtAssessment', 'PtLabResult', 'PtDemographic']
+    interventions_attributes = dict()
+    
+    for table in database_tables:
+        interventions_attributes[table] = pd.read_excel('COVID_interventions_attributes.xlsx', sheet_name='PtAssessment')
+        sql = """SELECT P.encounterId as encounterId, P.chartTime, DI.longLabel, DA.shortLabel, P.valueNumber, P.valueString, DI.interventionId as interventionId, DA.attributeId as attributeId 
+        FROM
+        """ + table +  """ P
+        INNER JOIN D_Intervention DI
+        ON P.interventionId=DI.interventionId 
+        INNER JOIN D_Attribute DA
+        ON P.attributeId=DA.attributeId
+        WHERE P.encounterId in (SELECT encounterId from PtBedStay where outTime is null and clinicalUnitId in (5,8)) AND DI.interventionId in 
+        """ + str(tuple(interventions_attributes[table].interventionId)) + " AND DA.attributeId in " + str(tuple(interventions_attributes[table].attributeId))
+        
+    ptassessment_results = icca_query(sql)
+    
 
     df_l = pd.read_excel('COVID_interventions_attributes.xlsx', sheet_name='PtLabresult')
     sql = """SELECT P.encounterId as encounterId, P.chartTime, DI.longLabel, DA.shortLabel, P.valueNumber, P.valueString, DI.interventionId as interventionId, DA.attributeId as attributeId FROM PtLabResult P
@@ -30,36 +42,84 @@ if __name__=='__main__':
     
     ptlabresult_results = icca_query(sql)
     
-#    
-#    sql = "SELECT * FROM UHB.SOFA_python"
-#    sofa = icca_query(sql, db='CISReportingDB')
+    
+    sql = "SELECT * FROM UHB.SOFA_python"
+    sofa = icca_query(sql, db='CISReportingDB')
 #    #print(sofa)
     
     sql = 'SELECT bedLabel, encounterId FROM PtBedStay WHERE outTime is null and clinicalUnitId in (5,8)'
     beds = icca_query(sql)
     
-    # Need to collapse columns based on priority
+    
     # Need to combine FiO2 across PtAssessment and PtLabResults dataframes
     # Need to compute Ventilator parameters from Kieron/Stefan formulae
     # Need to work out what to do about 'No Bed' patients - some are valid, some are not?
     # Need to add valueNumber or valueString indicator to excel file. 
+    # Need to set time out when scheduling this on server...if timeout, revert to old josn?
     
-    # see @Select row by max value in group (link in jupyter notebook)@
+    # need a clever way of looping over variables that are in the schema, working out whether to extract them from PtAssessment etc, or compute them,and if they are stored in valueString or valueNumber, and how to apply condition formatting, and get previous value 
     
-    patient_data = dict()
+    # need a celever way of defining stylings..
+    ## Need two levels of script/task for the timeout scheduling? (otheriwse might timeout during write of new json and corrupt the file -> add if corrupt fn to front end?)
     
-    for bed,encounter in zip(beds.bedLabel,beds.encounterId):
-        patient_data[bed] = dict()
+    def get_derived_value(parameter):
+        return None
+    
+    def get_parameter_value_dict(parameter, bed_data_points, patient_labs, patient_assess):
         
+        if parameter['type'] == 'PtLabResult':
+            
+            value = patient_labs.loc[patient_labs.vname==parameter['name']].valueNumber
+            value = value.iloc[0] if len(value)>0 else None
+                
+        elif parameter['type'] == 'PtAssessment':
+            
+            value = patient_assess.loc[patient_assess.vname==parameter['name']].valueNumber
+            value = value.iloc[0] if len(value)>0 else None
+        
+        elif parameter['type'] == 'derived':
+            value = get_derived_value(parameter)
+        else:
+            value = None
+            
+        # This doesn't work - need to get the list of data points corresponding to this bed...
+        names = [point['name'] for point in bed_data_points] if bed_data_points is not None else []    
+        previous = bed_data_points[names.index(parameter['name'])]['value'] if parameter['name'] in names else None
+            
+        
+        return {'name': parameter['name'],
+                   'value': value,
+                   'style': 'normal', # NEED TO DEFINE STYLING (IN EXCEL?)
+                   'previous': previous
+                   }
+        
+    def build_bed_value_dict(bedId, encounterId, schema):
+        bvd = {'bedId': bedId, 'dataPoints': []}
+    
         patient_labs = ptlabresult_results.loc[ptlabresult_results.encounterId==encounter]
         patient_labs = patient_labs.merge(df_l, on=['interventionId', 'attributeId'], how='inner')
-        
         patient_labs = patient_labs.loc[patient_labs.reset_index().groupby('vname')['priority'].idxmax()]
         
-        for v in df_l.vname:
-            patient_data[bed][v] = patient_labs.loc[patient_labs.vname==v].valueNumber
-           
+        patient_assess = ptassessment_results.loc[ptassessment_results.encounterId==encounter]
+        patient_assess = patient_assess.merge(df_a, on=['interventionId', 'attributeId'], how='inner')
+        patient_assess = patient_assess.loc[patient_assess.reset_index().groupby('vname')['priority'].idxmax()]
+        
+        beds = [value['bedId'] for value in schema['values']]
+        bed_data_points = schema['values'][beds.index(bedId)]['dataPoints'] if bedId in beds else None
+        
+        for parameter in schema['parameters']:
             
-    print(patient_data)
+            bvd['dataPoints'].append(get_parameter_value_dict(parameter, bed_data_points, patient_labs, patient_assess)) 
+            
+        return bvd
+       
+    
+    for bed,encounter in zip(beds.bedLabel,beds.encounterId):
+        data_dict['values'].append(build_bed_value_dict(bed, encounter, schema))
+         
+
+    data_dict['parameters'] = schema['parameters']
+    with open('patient_data.json', 'w') as outfile:
+        json.dump(data_dict, outfile)
     
             
